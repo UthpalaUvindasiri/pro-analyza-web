8f9f2b9f163dc620d3a123f87c3092fd79903551
Supun <hsupunw@gmail.com>
2018-06-16 17:44:07 +0530

    add code analysis ui

---
+++ src/app/code-analysis/code-analysis.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-code-analysis',
+  templateUrl: './code-analysis.component.html',
+  styleUrls: ['./code-analysis.component.css']
+})
+export class CodeAnalysisComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+
+  function void populate(string value)
+  {
+
+  }
+
+}
